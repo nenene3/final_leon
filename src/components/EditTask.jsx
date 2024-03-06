51a62194666d074fb74ab5e07e2fca76e3e7e14d
nenene3 <gilweiss971@gmail.com>
2024-03-06 09:52:38 +0200

    add the function to edit todo

--- src/components/EditTask.jsx
+++ src/components/EditTask.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+import { doc, getDoc, updateDoc, deleteDoc } from "firebase/firestore";
+import { db } from "../firebase-config";
+export default function EditTask(props) {
+  let [d, setD] = useState("");
+  let [name1, setName] = useState("");
+  let updateStatus = async (name) => {
+    try {
+      let docChange = doc(db, props.user, name);
+      const docSnap = await getDoc(docChange);
 
-export default function EditTask() {
+      let a = await updateDoc(docChange, {
+        taskName: name1,
+        details: d,
+      });
+      window.location.reload();
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
-    <div>EditTask</div>
-  )
+    <div>
+      <input
+        placeholder={props.taskName}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      ></input>
+      <input
+        placeholder={props.details}
+        onChange={(e) => {
+          setD(e.target.value);
+        }}
+      ></input>
+      <button onClick={() => updateStatus(props.id)}>submit</button>
+    </div>
+  );
 }
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import { doc, getDoc, updateDoc, deleteDoc } from "firebase/firestore";
 import { db } from "../firebase-config";
+import EditTask from "./EditTask";
+import './a.css'
 export default function Task(props) {
   let [done, setDone] = useState(props.isComplete);
+  let [edit,setEdit] = useState(false);
   let updateStatus = async (name) => {
     try {
       let docChange = doc(db, props.user, name);
@@ -26,25 +29,40 @@ export default function Task(props) {
   };
   return (
     <div>
-      <div
-        title={props.modify}
-        className={`${
-          done ? "finish" : `${props.i % 2 == 0 ? "task1" : "task2"}`
-        }`}
-        onClick={(e) => {
-          e.stopPropagation()
-          updateStatus(props.id);
-          setDone(!done);
-        }}
-      >
-        <p>{props.taskName}</p>
-        <p>{props.details}</p>
+      {edit ? (
+        <EditTask 
+          taskName={props.taskName} details={props.details} user={props.user} id={props.id}
+        />
+      ) : (
+        <div>
+          <div
+            title={props.modify}
+            className={`${
+              done ? "finish" : `${props.i % 2 == 0 ? "task1" : "task2"}`
+            }`}
+            onClick={(e) => {
+              e.stopPropagation();
+              updateStatus(props.id);
+              setDone(!done);
+            }}
+          >
+            <p>{props.taskName}</p>
+            <p>{props.details}</p>
 
-        <h3>{done ? "finish" : "notfinish"}</h3>
-        <br />
-        <button onClick={(e) => {remove(props.id) ;e.stopPropagation() }}>remove</button>
-        <button>edit</button>
-      </div>
+            <h3>{done ? "finish" : "notfinish"}</h3>
+            <br />
+            <button
+              onClick={(e) => {
+                remove(props.id);
+                e.stopPropagation();
+              }}
+            >
+              remove
+            </button>
+          </div>
+        </div>
+      )}
+      <button onClick={() => setEdit(!edit)}>edit</button>
     </div>
   );
 }
